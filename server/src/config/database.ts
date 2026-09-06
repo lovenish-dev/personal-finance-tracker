d74725b71692a07bfb8292d306d585a36d87e223
Lovenish <lovenish.build@gmail.com>
2026-09-06 16:59:24 +0530

    fix: configure production database connection

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,14 +1,21 @@
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
-    port: Number(process.env.DB_PORT) || 5432
-})
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT) || 5432,
+    });
 
 export default pool;
